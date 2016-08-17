219743e53ec34f67c55c5e4953c2d199805eb6fc
Julien Acroute <julien.acroute@camptocamp.com>
2016-08-17 09:44:39 +0200

    Remove unused imports
    org.json is not used is this project and is not import by pom.xml

--- src/main/java/com/camptocamp/gs/tools/postgistojndi/PostgisToJNDITransformer.java
+++ src/main/java/com/camptocamp/gs/tools/postgistojndi/PostgisToJNDITransformer.java
@@ -7,9 +7,6 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.XML;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
